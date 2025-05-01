59aff9b4e2b50806517966640292f568f05ac619
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-05-01 17:54:30 +0530

    view logic

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -1,8 +1,15 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello,),
-    path('menu/<int: menu_id>',views.menu_by_id,name='menu_by_id'),
+    path('', views.home, name='home'),
+    path('say_hello/', views.say_hello, name='say_hello'),
+    path('display_date/', views.display_date, name='display_date'),
+    path('menu/', views.menu, name='menu'),
+    path('menu/<int:menu_id>/', views.menu_by_id, name='menu_by_id'),
+
+
+    # Class-Based Views (CBVs)
+    path('menus/', views.MenuListView.as_view(), name='menu_list'),
+    path('myview/', views.MyView.as_view(), name='my_view'),
 ]
--- LittleLemon/views.py
+++ LittleLemon/views.py
@@ -1,18 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from datetime import datetime
 from .models import Menu
-# Create your views here.
+from django.views import View
+from django.views.generic import ListView
 
-from django.shortcuts import render
+
+# Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
-
-def menu_by_id(request,menu_id):
-    menu = Menu.objects.get(pk=menu_id)
+def menu_by_id(request, menu_id):
+    # menu = Menu.objects.get(pk=menu_id)
+    menu = get_object_or_404(Menu, pk=menu_id)
 
     # return HttpResponse(f"{menu.menu_item}:Type of {menu.cuisine} cuisine")
-    return render(request, 'menu_card.html',{'menu':menu})
+    return render(request, 'menu_card.html', {'menu': menu})
+ 
+#  MVT 
+def say_hello(request):
+    return HttpResponse("Welcome to Little Lemon Restaurant")
+
+def display_date(request):
+    date_joined = datetime.today().year
+    return HttpResponse(date_joined)
+
+def menu(request):
+    text = """<h1 style="color: #F4CE14;"> This is Little Lemon again !</h1>"""
+    return HttpResponse(text)
+
+
+# Views Logic 
+# Class-Based View (CBV) for displaying menus as a list
+
+class MenuListView(ListView):
+    model = Menu
+    template_name = 'menu_list.html'
+    context_object_name = 'menus'
+
+# Class-Based View (CBV) for handling GET and POST requests
+class MyView(View): 
+    def get(self, request): 
+        # Logic to process GET request
+        return HttpResponse('Response to GET request')
+
+    def post(self, request): 
+        # Logic to process POST request
+        return HttpResponse('Response to POST request')
+
+
 
